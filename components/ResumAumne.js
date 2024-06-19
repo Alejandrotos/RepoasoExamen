b7428dd172017c88eee64067684fe3745aa68729
Alejandrotos <altove@floridauniversitaria.es>
2024-06-19 19:59:42 +0200

    hola

--- components/ResumAumne.js
+++ components/ResumAumne.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import {StyleSheet, Text, View, Dimensions, ScrollView} from 'react-native';
+import {StyleSheet, Text, View, Dimensions, ScrollView, TouchableOpacity} from 'react-native';
 import {Surface} from 'react-native-paper';
 
 const ResumAlumne = props => {
-  <View>
+  return(
+  <TouchableOpacity>
     <Text>{props.nom}</Text>
     <View style={styles.estilBorde}>
       <Text>Nota</Text>
       <Text>{props.nota}</Text>
     </View>
-  </View>;
+  </TouchableOpacity>
+  );
 };
 
 const styles = StyleSheet.create({
